32f866082529eedb09030cf1ad2003a87d970840
Tiruum <selintimur@yandex.ru>
2024-05-20 03:57:18 +0300

    Gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Устанавливаем базовый путь на корень
+  build: {
+    outDir: 'dist', // Папка, куда будут собраны файлы
+    assetsDir: 'assets', // Папка, куда будут скопированы ресурсы
+    assetsInlineLimit: 0, // Отключаем встраивание ресурсов
+  },
+});
